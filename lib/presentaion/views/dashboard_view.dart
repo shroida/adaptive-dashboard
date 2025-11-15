f20d1b330ab171924b089c659707771439e85126
shroida <shroida012@gmail.com>
2025-11-15 09:50:02 +0200

    setup dashboard view

---
+++ lib/presentaion/views/dashboard_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class DashboardView extends StatefulWidget {
+  const DashboardView({Key? key}) : super(key: key);
+
+  @override
+  DashboardViewState createState() => DashboardViewState();
+}
+
+class DashboardViewState extends State<DashboardView> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Tab');
+  }
+}
