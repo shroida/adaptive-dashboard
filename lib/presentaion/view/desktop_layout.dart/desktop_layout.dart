617c97287ed20e9022f7f4615edca3ffb2cbbd19
shroida <shroida012@gmail.com>
2025-11-15 14:29:45 +0200

    extract LineChartWidget

--- lib/presentaion/view/desktop_layout.dart/desktop_layout.dart
+++ lib/presentaion/view/desktop_layout.dart/desktop_layout.dart
@@ -2,6 +2,7 @@ import 'package:adaptive_dashboard/presentaion/widgets/bar_chart_widget.dart';
 import 'package:adaptive_dashboard/presentaion/widgets/dashboard_card.dart';
 import 'package:adaptive_dashboard/presentaion/widgets/drawer_menu.dart';
 import 'package:adaptive_dashboard/presentaion/widgets/header_middle_widget.dart';
+import 'package:adaptive_dashboard/presentaion/widgets/line_chart_widget.dart';
 import 'package:flutter/material.dart';
 
 class DesktopLayout extends StatelessWidget {
@@ -84,7 +85,6 @@ class DesktopLayout extends StatelessWidget {
             ),
           ),
 
-          // Right Sidebar
           Expanded(
             flex: 1,
             child: Padding(
@@ -115,67 +115,4 @@ class DesktopLayout extends StatelessWidget {
       ),
     );
   }
-}
-
-// -------------------- LINE CHART --------------------
-class LineChartWidget extends StatelessWidget {
-  const LineChartWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final data = [50.0, 80.0, 30.0, 70.0, 90.0]; // Example data
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(25),
-        boxShadow: const [
-          BoxShadow(color: Colors.black12, blurRadius: 8, offset: Offset(0, 5))
-        ],
-      ),
-      padding: const EdgeInsets.all(16),
-      child: CustomPaint(
-        painter: LineChartPainter(data),
-        child: Container(),
-      ),
-    );
-  }
-}
-
-class LineChartPainter extends CustomPainter {
-  final List<double> data;
-  LineChartPainter(this.data);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.orange
-      ..strokeWidth = 4
-      ..style = PaintingStyle.stroke;
-
-    final path = Path();
-    final gap = size.width / (data.length - 1);
-    for (int i = 0; i < data.length; i++) {
-      final x = i * gap;
-      final y = size.height - (data[i] / 100 * size.height);
-      if (i == 0) {
-        path.moveTo(x, y);
-      } else {
-        path.lineTo(x, y);
-      }
-    }
-    canvas.drawPath(path, paint);
-
-    // Draw points
-    final pointPaint = Paint()
-      ..color = Colors.red
-      ..strokeWidth = 6;
-    for (int i = 0; i < data.length; i++) {
-      final x = i * gap;
-      final y = size.height - (data[i] / 100 * size.height);
-      canvas.drawCircle(Offset(x, y), 4, pointPaint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
---
+++ lib/presentaion/widgets/line_chart_widget.dart
@@ -0,0 +1,64 @@
+
+import 'package:flutter/material.dart';
+
+class LineChartWidget extends StatelessWidget {
+  const LineChartWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final data = [50.0, 80.0, 30.0, 70.0, 90.0]; // Example data
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: const [
+          BoxShadow(color: Colors.black12, blurRadius: 8, offset: Offset(0, 5))
+        ],
+      ),
+      padding: const EdgeInsets.all(16),
+      child: CustomPaint(
+        painter: LineChartPainter(data),
+        child: Container(),
+      ),
+    );
+  }
+}
+
+class LineChartPainter extends CustomPainter {
+  final List<double> data;
+  LineChartPainter(this.data);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.orange
+      ..strokeWidth = 4
+      ..style = PaintingStyle.stroke;
+
+    final path = Path();
+    final gap = size.width / (data.length - 1);
+    for (int i = 0; i < data.length; i++) {
+      final x = i * gap;
+      final y = size.height - (data[i] / 100 * size.height);
+      if (i == 0) {
+        path.moveTo(x, y);
+      } else {
+        path.lineTo(x, y);
+      }
+    }
+    canvas.drawPath(path, paint);
+
+    // Draw points
+    final pointPaint = Paint()
+      ..color = Colors.red
+      ..strokeWidth = 6;
+    for (int i = 0; i < data.length; i++) {
+      final x = i * gap;
+      final y = size.height - (data[i] / 100 * size.height);
+      canvas.drawCircle(Offset(x, y), 4, pointPaint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
