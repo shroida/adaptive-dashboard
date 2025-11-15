b76eaa132a27dbb7c342bdb3179332db57b6033e
shroida <shroida012@gmail.com>
2025-11-15 11:20:20 +0200

    enhance drawer Item

--- lib/presentaion/widgets/drawer_widget.dart
+++ lib/presentaion/widgets/drawer_widget.dart
@@ -1,49 +1,94 @@
 import 'package:flutter/material.dart';
 
-Widget drawerItem(IconData icon, String title, {bool isSelected = false}) {
-  return Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
-    child: Card(
-      elevation: isSelected ? 4 : 0,
-      margin: EdgeInsets.zero,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-        side: isSelected
-            ? BorderSide(color: Colors.blue.shade200, width: 1)
-            : BorderSide.none,
-      ),
-      color: isSelected ? const Color(0xFFE0E0FF) : Colors.white,
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          onTap: () {},
-          hoverColor: Colors.blue.withOpacity(0.1),
-          splashColor: Colors.blue.withOpacity(0.2),
-          borderRadius: BorderRadius.circular(12),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-            child: Row(
-              children: [
-                Icon(
-                  icon,
-                  size: 22,
-                  color: isSelected ? Colors.blue : Colors.grey[700],
+class DrawerItem extends StatefulWidget {
+  final IconData icon;
+  final String title;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const DrawerItem({
+    super.key,
+    required this.icon,
+    required this.title,
+    this.isSelected = false,
+    required this.onTap,
+  });
+
+  @override
+  State<DrawerItem> createState() => _DrawerItemState();
+}
+
+class _DrawerItemState extends State<DrawerItem> {
+  bool _isHovering = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Color backgroundColor;
+
+    if (widget.isSelected) {
+      backgroundColor = const Color(0xFFE0E0FF);
+    } else if (_isHovering) {
+      backgroundColor = Colors.blue.withOpacity(0.05);
+    } else {
+      backgroundColor = Colors.white;
+    }
+
+    final color =
+        widget.isSelected ? Colors.blue.shade700 : Colors.grey.shade700;
+
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovering = true),
+      onExit: (_) => setState(() => _isHovering = false),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeInOut,
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(12),
+            border: widget.isSelected
+                ? Border.all(color: Colors.blue.shade300)
+                : Border.all(color: Colors.transparent),
+            boxShadow: [
+              BoxShadow(
+                color: (widget.isSelected || _isHovering)
+                    ? Colors.black.withOpacity(0.08)
+                    : Colors.transparent,
+                blurRadius: 8,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: widget.onTap,
+              borderRadius: BorderRadius.circular(12),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                child: Row(
+                  children: [
+                    Icon(widget.icon, size: 24, color: color),
+                    const SizedBox(width: 14),
+                    Text(
+                      widget.title,
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: widget.isSelected
+                            ? FontWeight.bold
+                            : FontWeight.w500,
+                        color: color,
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(width: 12),
-                Text(
-                  title,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight:
-                        isSelected ? FontWeight.bold : FontWeight.normal,
-                    color: isSelected ? Colors.blue : Colors.grey[800],
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
